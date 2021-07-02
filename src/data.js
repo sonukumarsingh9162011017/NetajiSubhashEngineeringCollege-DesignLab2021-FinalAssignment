80712f68f2181c2b90af7a9b3bb2c0e51f5dc053
sonu <aryasinghsonu2607@gmail.com>
2021-07-02 18:12:16 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Data Structure and Algorithms',
+        price: 570,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/41vXBnhzmXL._SX327_BO1,204,203,200_.jpg',
+      },
+      {
+        id: '2',
+        name: 'Operating System',
+        price: 637,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/41vr8m6x8SL._SX362_BO1,204,203,200_.jpg',
+      },
+      {
+        id: '3',
+        name: 'Java',
+        price: 835,
+        image: 'https://rukminim1.flixcart.com/image/832/832/kjbr8280-0/book/l/j/q/java-2-original-imafyxd8ffxjcquv.jpeg?q=70',
+      },
+      {
+        id: '4',
+        name: 'Machine Learning',
+        price: 920,
+        image: 'https://m.media-amazon.com/images/I/51GxW8immiL.jpg',
+      },
+    ],
+  };
+  export default data;
+  
